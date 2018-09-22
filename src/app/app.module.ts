69ffa556d99c143ba976bafa6667a64411536b8d
eleanor <eleanorcjkgraham@gmail.com>
2018-09-21 22:02:43 -0400

    changing the order of session indicator and step progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { MatButtonModule, MatCardModule, MatDividerModule, MatIconModule, MatProgressBarModule, MatToolbarModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDividerModule,
+  MatIconModule,
+  MatProgressBarModule,
+  MatToolbarModule
+} from '@angular/material';
 import { ScenarioComponent } from './scenario/scenario.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MissingLetterComponent } from './missing-letter/missing-letter.component';
@@ -16,12 +23,12 @@ import { HttpClientModule} from '@angular/common/http';
 import { StepComponent } from './step/step.component';
 import { PageComponent } from './page/page.component';
 import { ControlConditionComponent } from './control-condition/control-condition.component';
-import {ProgressBarComponent} from './progress-bar/progress-bar.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { ThoughtbubbleComponent } from './thoughtbubble/thoughtbubble.component';
 import { HighlightComponent } from './highlight/highlight.component';
 import { TrainingComponent } from './training/training.component';
 import { BulletListComponent } from './bullet-list/bullet-list.component';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
