f5e06bc6a6731e3851ee0f16e41478926ef04082
Dan Funk <daniel.h.funk@gmail.com>
2020-06-12 07:42:06 -0400

    Remove image/title page. Make the interval on the counter happen every 250 milliseconds, to remove stutter.

--- src/app/recognition-ratings/recognition-ratings.component.ts
+++ src/app/recognition-ratings/recognition-ratings.component.ts
@@ -1,12 +1,13 @@
 import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
 import { ApiService } from '../api.service';
-import {Scenario, Session, Step, Study} from '../interfaces';
+import {Page, Scenario, Session, Step, Study} from '../interfaces';
 // import { environment } from '../../environments/environment';
 import { ActivatedRoute } from '@angular/router';
 import {Round} from '../round';
 // import {catchError, map, withLatestFrom} from 'rxjs/operators';
 import {combineLatest, observable, Observable} from 'rxjs';
 import {withLatestFrom} from 'rxjs/operators';
+import {environment} from '../../environments/environment';
 
 enum State {
   'INTRO', 'RR'
@@ -91,6 +92,14 @@ export class RecognitionRatingsComponent implements OnInit {
   loadTraining() {
     this.api.getTrainingCSV('recognitionRatings').subscribe(scenarios => {
       this.scenarios = scenarios;
+
+      // Drop the first page containing the title, and make it a header for the subsequent
+      for (const s of scenarios) {
+        const title: Page = s.pages[0];
+        s.pages.shift(); // Drop the title page.
+        s.pages[0].elements.unshift({type: 'Header', content: title.elements[0].content});
+        console.log(s);
+      }
       this.currentScenario = this.scenarios[0];
     });
   }
@@ -98,6 +107,9 @@ export class RecognitionRatingsComponent implements OnInit {
   scenarioComplete($event) {
     this.scenarioIndex++;
     this.currentScenario = this.scenarios[this.scenarioIndex];
+    if (this.scenarioIndex >= this.scenarios.length) {
+      window.location.href = environment.redirect;
+    }
   }
 
   updatePageCount(event) {
